3e3fbd0692934a6ae60ffee4006101da95501939
DanielMarella <marella.daniel96@gmail.com>
2023-06-07 12:13:16 +0200

    recreated the structure of the exercise with css and html

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,15 @@ const { createApp } = Vue
     createApp({
     data() {
         return {
-        message: 'Hello Vue!'
+
         }
     }
 }).mount('#app')
+
+
+
+
+// Milestone 1
+// ●	Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+
+// ●	Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
