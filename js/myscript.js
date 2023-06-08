0fcd2417de6c80408261b617e7c2151f7b77a236
DanielMarella <marella.daniel96@gmail.com>
2023-06-08 12:52:33 +0200

    thinking how to add the message dinamically

--- js/myscript.js
+++ js/myscript.js
@@ -166,16 +166,16 @@ const { createApp } = Vue
                         }
                     ],
                 }
-            ]
+            ],
+            
+            
+            console,log(contacts){}
             
             
             
             
             
-
-
-
-
+            
         }
     }
 }).mount('#app')
@@ -183,7 +183,9 @@ const { createApp } = Vue
 
 
 
-// Milestone 1
-// ●	Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Milestone 2
 
-// ●	Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+// ●	Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+
+// ●	Click sul contatto mostra la conversazione del contatto cliccato
+
