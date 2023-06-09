4ea9a14053974018c13f13ed58c8d6c6457988e7
DanielMarella <marella.daniel96@gmail.com>
2023-06-09 16:02:05 +0200

    mileston 3 part 1 done for every user

--- js/myscript.js
+++ js/myscript.js
@@ -183,8 +183,8 @@ const { createApp } = Vue
 
 
         addNewMessage(message){
-            this.contacts[0].messages.push({message : message, status : 'sent'});
-            console.log(this.contacts[0].messages)
+            this.contacts[this.activeIndex].messages.push({message : message, status : 'sent'});
+            console.log(this.contacts[this.activeIndex].messages)
         }
 
     }
@@ -194,10 +194,8 @@ const { createApp } = Vue
 
 
 
+// Milestone 3
+// ●	Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// ●	Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
-// Milestone 2
-
-// ●	Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-
-// ●	Click sul contatto mostra la conversazione del contatto cliccato
 
